dda12cfa244bbfdbe91bc279cad4467a5d95808f
atcampbell <adamtoddcampbell@gmail.com>
2023-07-23 22:36:29 +0100

    add v1 rungler

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 async function setup() {
-    const patchExportURL = "export/patch.export.json";
+    const patchExportURL = "export/rungler-web.export.json";
 
     // Create AudioContext
     const WAContext = window.AudioContext || window.webkitAudioContext;
